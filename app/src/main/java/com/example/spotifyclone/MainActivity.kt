4622371a19a7e261e67d2f981582ecc802582460
Smit <2001smitpatel@gmail.com~>
2023-09-24 16:10:17 +0530

    i

--- app/src/main/java/com/example/spotifyclone/MainActivity.kt
+++ app/src/main/java/com/example/spotifyclone/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.spotifyclone
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.spotifyclone.databinding.ActivityMainBinding
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,19 +23,24 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnHome.setOnClickListener {
             loadFragment(HomeFragment())
+            change_btnState(binding.btnHome,R.drawable.icon_home_filled)
         }
 
         binding.btnSearch.setOnClickListener{
             loadFragment(SearchFragment())
+            change_btnState(binding.btnSearch,R.drawable.icon_search_filled)
         }
 
         binding.btnLibrary.setOnClickListener(){
             loadFragment(LibraryFragment())
+            change_btnState(binding.btnLibrary,R.drawable.icon_library_filled)
         }
 
         binding.btnPremium.setOnClickListener{
             loadFragment(PremiumFragment())
+            change_btnState(binding.btnPremium,R.drawable.icon_premium_filled)
         }
+
     }
 
     private fun loadFragment(fragment: Fragment){
@@ -41,4 +49,14 @@ class MainActivity : AppCompatActivity() {
         ft.replace(R.id.frag_container,fragment)
         ft.commit()
     }
+
+    private fun change_btnState(btn: MaterialButton,icon: Int){
+        btn.icon =  ContextCompat.getDrawable(this,icon)
+        btn.setTextColor(ContextCompat.getColorStateList(this, R.color.white))
+    }
+//    private fun change_btnState(btn: MaterialButton) {
+//        val color = ContextCompat.getColor(this, R.color.white) // Replace with your desired color resource
+//        btn.iconTint = ColorStateList.valueOf(color)
+//        btn.setTextColor(ColorStateList.valueOf(color))
+//    }
 }
